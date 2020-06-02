cb14434485da22875fcec3938141bd6242ac5c8e
Alex Sun <alexsun82@icloud.com>
2020-06-02 23:00:31 +0200

    Inplement adding categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const validator = require("express-validator");
 
 exports.catList = (req, res, next) => {
     res.send("NOT IMPLEMENTED: Category list");
@@ -9,12 +10,41 @@ exports.catDetail = (req, res, next) => {
 };
 
 exports.catAddGet = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Add category GET");
+    res.render("category_form", { title: "Add Category" });
 };
 
-exports.catAddPost = (req, res, next) => {
-    res.send("NOT IMPLEMENTED: Add category POST");
-};
+exports.catAddPost = [
+    validator.check("name").trim().isLength({ min: 1 }),
+    validator.check("description").trim().isLength({ min: 1 }),
+    validator.body("name").escape(),
+    validator.body("description").escape(),
+
+    async (req, res, next) => {
+        const errors = validator.validationResult(req);
+        const category = new Category({ name: req.body.name, description: req.body.description });
+        if (!errors.isEmpty) {
+            res.render("category_form", { title: "Add Category", category: category, errors: errors.array() });
+            return void 0;
+        } else {
+            await Category.findOne({ name: req.body.name })
+                .exec((err, result) => {
+                    if (err) {
+                        return next(err);
+                    }
+                    if (result) {
+                        res.redirect(result.url);
+                    } else {
+                        category.save(err => {
+                            if (err) {
+                                return next(err);
+                            }
+                            res.redirect(category.url);
+                        })
+                    }
+                });
+        }
+    }
+];
 
 exports.catDeleteGet = (req, res, next) => {
     res.send("NOT IMPLEMENTED: Delete category GET");
