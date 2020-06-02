2f0024755c73be5922649378ae07b4e112cad3dc
Alex Sun <alexsun82@icloud.com>
2020-06-02 11:36:09 +0200

    Add controllers for categories + items

---
+++ controllers/categoryController.js
@@ -0,0 +1,33 @@
+const Category = require("../models/category");
+
+exports.catList = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Category list");
+};
+
+exports.catDetail = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Category detail page");
+};
+
+exports.catAddGet = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Add category GET");
+};
+
+exports.catAddPost = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Add category POST");
+};
+
+exports.catDeleteGet = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Delete category GET");
+};
+
+exports.catDeletePost = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Delete category POST");
+};
+
+exports.catUpdateGet = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Update category GET");
+};
+
+exports.catUpdatePost = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Update category POST");
+};
---
+++ controllers/itemController.js
@@ -0,0 +1,33 @@
+const Item = require("../models/item");
+
+exports.itemList = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Item list");
+};
+
+exports.itemDetail = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Item detail page");
+};
+
+exports.itemAddGet = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Add Item GET");
+};
+
+exports.itemAddPost = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Add Item POST");
+};
+
+exports.itemDeleteGet = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Delete Item GET");
+};
+
+exports.itemDeletePost = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Delete Item POST");
+};
+
+exports.itemUpdateGet = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Update Item GET");
+};
+
+exports.itemUpdatePost = (req, res, next) => {
+    res.send("NOT IMPLEMENTED: Update Item POST");
+};
