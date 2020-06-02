25e352e614a9f03680fad2ca31d44e850ce81c03
Alex Sun <alexsun82@icloud.com>
2020-06-02 10:19:32 +0200

    Configure sass middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
+const mongoose = require("mongoose");
 
 const indexRouter = require('./routes/index');
 
@@ -22,13 +23,12 @@ app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+  indentedSyntax: false, // true = .sass and false = .scss
+  sourceMap: false
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
